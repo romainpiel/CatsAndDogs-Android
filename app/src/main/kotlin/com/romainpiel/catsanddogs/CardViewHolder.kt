6122ec629c545c60a6128a3979b1641e147063e5
Romain Piel <romain.piel@gmail.com>
2017-04-17 16:54:12 +0100

    styled cards

--- app/src/main/kotlin/com/romainpiel/catsanddogs/CardViewHolder.kt
+++ app/src/main/kotlin/com/romainpiel/catsanddogs/CardViewHolder.kt
@@ -6,13 +6,22 @@ import android.widget.TextView
 
 class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val titleTextView: TextView
+    val subtitleTextView: TextView
+    val dateTextView: TextView
+    val timeTextView: TextView
 
     init {
         titleTextView = itemView.findViewById(R.id.title) as TextView
+        subtitleTextView = itemView.findViewById(R.id.subtitle) as TextView
+        dateTextView = itemView.findViewById(R.id.date) as TextView
+        timeTextView = itemView.findViewById(R.id.time) as TextView
     }
 
     fun bind(item: Card) {
         titleTextView.text = item.title
+        subtitleTextView.text = item.subtitle
+        dateTextView.text = item.date
+        timeTextView.text = item.time
     }
 
 }
